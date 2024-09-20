e082cfafc6a7d5f97cf6456d1685226e7661fdab
Joel Pablo <dgtlmonk@Joels-MacBook-Pro.local>
2024-09-19 18:10:49 -0600

    feat: ppr (1)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-import { Postpone } from 'next/dist/server/app-render/dynamic-rendering';
-
 const nextConfig = {
   experimental: {
     ppr: 'incremental'
